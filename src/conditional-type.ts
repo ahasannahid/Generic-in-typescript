721dba1501ff6e5c35efe7c354c9f557ebe0cad1
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-06-12 22:44:33 +0600

    nested conditional typing

--- src/conditional-type.ts
+++ src/conditional-type.ts
@@ -1,6 +1,12 @@
 // when a type is dependent on another type is called conditional type.
 
-type a1 = string;
+type a1 = number;
+type a3 = undefined;
+type a4 = number;
+
+// conditional typing
 type a2 = a1 extends string ? string : null;
 
-// ne
+// nested conditional typing
+type c = a1 extends null ? null : a3 extends number ? number : any
+type d = a1 extends null ? null : a3 extends number ? number : a4 extends null ? null : never;
