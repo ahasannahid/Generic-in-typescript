57bcd1a11540f3a82fb8c5f16d1774d0e50477c4
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-06-12 17:15:48 +0600

    generic key of

---
+++ src/generic-keyOf-constraints.ts
@@ -0,0 +1,31 @@
+type PersonType = {
+    name : string,
+    age : number,
+    address : string
+};
+
+type newType = 'name' | 'age' | 'address'    //Manually kora hoise ekhane (union)
+
+// keyOf operator
+type newTypeusingKeyOf = keyof PersonType;
+
+// const a : newType = 'address'
+// const b : newTypeusingKeyOf = 'age'
+
+// ({name: 'Mr.X', age: 100}, 'name')  //Mr. X
+// ({name: 'Mr.X', age: 100}, 'age')  //100
+
+function getProperty<X , Y extends keyof X>(obj:X, key: Y){
+    obj[key]
+}
+
+const property = getProperty({name: 'Mr.X', age:100}, 'age')
+
+
+// bracket notation
+// const a = {
+//     name: 'Mr.X',
+//     age: 100
+// }
+// a['name']
+// a['age']
