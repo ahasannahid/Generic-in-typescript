04411599e485d60c1347e1d6c31017f6b748093b
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-06-13 10:49:26 +0600

    more example

--- src/mapped-type.ts
+++ src/mapped-type.ts
@@ -2,3 +2,33 @@ const arrayofNumbers = [1,2,3,4]   //convert kore string array korte cacchi ['1'
 
 const arrayofStrings = arrayofNumbers.map((number) => number.toString());
 console.log(arrayofStrings);
+
+type AreaNumber={
+    height: number;
+    width:number;
+};
+
+type AreaString = {
+    height: string;
+    width:string;
+}
+
+type AreaReadonly = {
+    readonly height: number;
+    readonly width:number;
+};
+
+const rectangularArea: AreaReadonly={
+    height: 10,
+    width: 12
+}
+
+rectangularArea.width=10;
+
+type A = AreaNumber['height']  //lookup types
+type B = keyof AreaNumber;  // 'width' | 'height' ekta union type toyari korbe
+
+// const obj = {
+//     name: 'Nahid'
+// }
+// obj['name'];
