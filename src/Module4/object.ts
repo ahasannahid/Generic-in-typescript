cb1f223bde7d159f75abdd449b9280882e6f1ddb
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-06-15 11:21:49 +0600

    property value change in class

--- src/Module4/object.ts
+++ src/Module4/object.ts
@@ -19,26 +19,23 @@
 //     }
 // }
 
-
+// class with parameter properties
 class Animal{
-    // public name : string;
-    // public species : string;
-    // public sound : string;
+    constructor(
+        public name: string,
+        public species: string,
+        public sound: string){}
 
-    // parameter properties
-    constructor(public name: string, public species: string, public sound: string){
-        // this.name = name
-        // this.species = species
-        // this.sound = sound
-    }
-
-    // class er vitor function declare korle setake method bole.ekhane arrow function use korbo na .karon arrow function e this property applicable hoy na.
     public makeSound(){
-        console.log(`The ${this.name} Says ${this.sound}`) //"German Shephard says geu geu"
+        console.log(`The ${this.name} Says ${this.sound}`)
     }
 }
 
 const dog = new Animal('German Shephard', 'dog', 'geu geu');
 const cat = new Animal('persian', 'cat', 'meu meu')
 dog.makeSound();
+cat.makeSound();
+// change property value
+cat.name = 'Billu';
+
 cat.makeSound();
