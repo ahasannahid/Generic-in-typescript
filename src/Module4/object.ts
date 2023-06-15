28c1f11f675c1fc432707cb0ee6c5d93cfc988f1
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-06-15 11:17:55 +0600

    parameter property in class

--- src/Module4/object.ts
+++ src/Module4/object.ts
@@ -1,17 +1,44 @@
 // class declare er somoy constructor method ke call korte hobe must.
-class Animal{
-    name : string;
-    species : string;
-    sound : string;
 
-    constructor(name: string, species: string, sound: string){
-        this.name = name
-        this.species = species
-        this.sound = sound
+// class declare er somoy constructor method ke call korte hobe must.
+// class Animal{
+//     public name : string;
+//     public species : string;
+//     public sound : string;
+
+//     
+//     constructor(name: string, species: string, sound: string){
+//         this.name = name
+//         this.species = species
+//         this.sound = sound
+//     }
+
+//     // class er vitor function declare korle setake method bole.ekhane arrow function use korbo na .karon arrow function e this property applicable hoy na.
+//     public makeSound(){
+//         console.log(`The ${this.name} Says ${this.sound}`) //"German Shephard says geu geu"
+//     }
+// }
+
+
+class Animal{
+    // public name : string;
+    // public species : string;
+    // public sound : string;
+
+    // parameter properties
+    constructor(public name: string, public species: string, public sound: string){
+        // this.name = name
+        // this.species = species
+        // this.sound = sound
     }
 
     // class er vitor function declare korle setake method bole.ekhane arrow function use korbo na .karon arrow function e this property applicable hoy na.
-    makeSound(){
+    public makeSound(){
         console.log(`The ${this.name} Says ${this.sound}`) //"German Shephard says geu geu"
     }
 }
+
+const dog = new Animal('German Shephard', 'dog', 'geu geu');
+const cat = new Animal('persian', 'cat', 'meu meu')
+dog.makeSound();
+cat.makeSound();
