eebb83e50d9f5b364a8d799dbefd2dc8d6637448
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-06-17 00:14:41 +0600

    abstraction basic

---
+++ src/Module4/abstraction.ts
@@ -0,0 +1,4 @@
+/**
+ * class er mjy je method thakbe tar real implementation somporke janbo na, sudu shape ta kmn buja jabe.implementation er details thakbena sudu output thakbe.
+ * abstraction duivabe kora jay. interface er maddhome. abstract class er maddhome
+ */
