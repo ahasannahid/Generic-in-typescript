750a135d54435129b1c709724eba8adc7ca4fa2b
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-06-16 23:23:03 +0600

    static class update

--- src/Module4/static.ts
+++ src/Module4/static.ts
@@ -1,21 +1,19 @@
 /**
- 
+ *static e this applicable na and static keyword use korte hobe. ekhetre constructor er proyojon neo. and instance create er somoy kono arguments pass korte hoyna. value class ei property decalere er somoy diye deua hoy. value accesser somoy class er property hisebe access korte hoy. method keu static kore deua deua jay. method ke static korle instance hisebe access kora jayna. instance create er dorkar porbena. direct class diye access kora jabe.
  */
 class Counter{
-    counter : number;
-    constructor(counter : number){
-        this.counter = counter
+   static counter : number = 0;
+    
+   static increment():number{
+     return Counter.counter =  Counter.counter + 1
     }
-    increment():number{
-     return this.counter =  this.counter + 1
-    }
-    decrement():number{
-     return this.counter =  this.counter - 1
+   static decrement():number{
+     return Counter.counter =  Counter.counter - 1
     }
 }
 
-const instance1 = new Counter (0);
-const instance2 = new Counter (1);
+// const instance1 = new Counter ();
+// const instance2 = new Counter ();
 
-console.log(instance1.increment())
-console.log(instance1.decrement())
+console.log(Counter.increment()); //value 0 theke 1 hoise
+console.log(Counter.increment()); //value 1 theke 2 hoise
