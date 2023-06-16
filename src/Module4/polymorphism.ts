abe3d52f6cd6aaffb3bfab725276a47a513324f9
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-06-16 23:59:55 +0600

    example of polymorphism

---
+++ src/Module4/polymorphism.ts
@@ -0,0 +1,78 @@
+/**
+ * ***class ke type hisebe use kora jay
+ * onek gulo rup thakbe.
+ * onekgulo class er same method different different output dekhab
+ * super sobsomoy this er age use hoy.
+ */
+
+class Person{
+    takenap():void{
+        console.log('I am sleeping 8 hours per day')
+    }
+}
+
+class Student extends Person{
+    takenap():void{
+        console.log('I am sleeping 10 hours per day')
+    }
+}
+
+class Developer extends Person{
+    takenap():void{
+        console.log('I am sleeping 5 hours per day')
+    }
+}
+
+function getNap(param: Person){
+    param.takenap();
+}
+
+// instance
+const person1 = new Person();
+const person2 = new Student();
+const person3 = new Developer();
+
+getNap(person1);
+getNap(person2);
+getNap(person3);
+
+
+
+class Shaps{
+    getArea():number{
+        return 0;
+    }
+}
+// area --> pi *r *r
+class Circle extends Shaps{
+    radius: number;
+    constructor (radius:number){
+        super()
+        this.radius = radius
+    }
+    getArea():number{
+        return Math.PI * this.radius * this.radius;
+    }
+}
+
+class Rectangle extends Shaps{
+    height : number;
+    width : number;
+    constructor(height: number, width : number){
+        super()
+        this.height = height
+        this.width = width
+    }
+    getArea(): number {
+        return this.width * this.height
+    }  
+}
+
+function getAreaOfShape(param:Shaps){
+    console.log(param.getArea())
+}
+
+getAreaOfShape(new Circle(10))
+getAreaOfShape(new Rectangle(10, 12))
+
+
--- src/Module4/static.ts
+++ src/Module4/static.ts
@@ -1,19 +1,19 @@
-/**
- *static e this applicable na and static keyword use korte hobe. ekhetre constructor er proyojon neo. and instance create er somoy kono arguments pass korte hoyna. value class ei property decalere er somoy diye deua hoy. value accesser somoy class er property hisebe access korte hoy. method keu static kore deua deua jay. method ke static korle instance hisebe access kora jayna. instance create er dorkar porbena. direct class diye access kora jabe.
- */
-class Counter{
-   static counter : number = 0;
+// /**
+//  *static e this applicable na and static keyword use korte hobe. ekhetre constructor er proyojon neo. and instance create er somoy kono arguments pass korte hoyna. value class ei property decalere er somoy diye deua hoy. value accesser somoy class er property hisebe access korte hoy. method keu static kore deua deua jay. method ke static korle instance hisebe access kora jayna. instance create er dorkar porbena. direct class diye access kora jabe.
+//  */
+// class Counter{
+//    static counter : number = 0;
     
-   static increment():number{
-     return Counter.counter =  Counter.counter + 1
-    }
-   static decrement():number{
-     return Counter.counter =  Counter.counter - 1
-    }
-}
+//    static increment():number{
+//      return Counter.counter =  Counter.counter + 1
+//     }
+//    static decrement():number{
+//      return Counter.counter =  Counter.counter - 1
+//     }
+// }
 
-// const instance1 = new Counter ();
-// const instance2 = new Counter ();
+// // const instance1 = new Counter ();
+// // const instance2 = new Counter ();
 
-console.log(Counter.increment()); //value 0 theke 1 hoise
-console.log(Counter.increment()); //value 1 theke 2 hoise
+// console.log(Counter.increment()); //value 0 theke 1 hoise
+// console.log(Counter.increment()); //value 1 theke 2 hoise
