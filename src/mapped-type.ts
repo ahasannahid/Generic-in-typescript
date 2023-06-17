0ff7f53baf8b0d135e76fd00beb56ffb4afd7f02
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-06-17 20:41:23 +0600

    omit type

--- src/mapped-type.ts
+++ src/mapped-type.ts
@@ -1,73 +1,73 @@
-// const arrayofNumbers = [1,2,3,4]   //convert kore string array korte cacchi ['1','2','3']
+// // const arrayofNumbers = [1,2,3,4]   //convert kore string array korte cacchi ['1','2','3']
 
-// const arrayofStrings = arrayofNumbers.map((number) => number.toString());
-// console.log(arrayofStrings);
+// // const arrayofStrings = arrayofNumbers.map((number) => number.toString());
+// // console.log(arrayofStrings);
 
-// type AreaNumber={
-//     height: number;
-//     width:number;
-// };
-// type AreaString = {
-//     height: string;
-//     width:string;
-// }
-// type AreaReadonly = {
-//     readonly height: number;
-//     readonly width:number;
-// };
-// const rectangularArea: AreaReadonly={
-//     height: 10,
-//     width: 12
-// }
-// // rectangularArea.width=10;
-// type A = AreaNumber['height']  //lookup types
-// type B = keyof AreaNumber;  // 'width' | 'height' ekta union type toyari korbe
-// // bracket notation of object
-// // const obj = {
-// //     name: 'Nahid'
+// // type AreaNumber={
+// //     height: number;
+// //     width:number;
+// // };
+// // type AreaString = {
+// //     height: string;
+// //     width:string;
 // // }
-// // obj['name'];
-// type Volume={
-//     height: number;
-//     width:number;
-//     depth:number;
-// };
-// // type Area = {
-// //     // [mapping] : type   //index signature
-// //     // [key in 'height' | 'width'] : number
-// //     // [key in keyof Volume] : number
-// //     // dynamic
-// //     [key in keyof Volume] : Volume[key]  //volume['height'] -->number;   volume['width'] -->number
+// // type AreaReadonly = {
+// //     readonly height: number;
+// //     readonly width:number;
+// // };
+// // const rectangularArea: AreaReadonly={
+// //     height: 10,
+// //     width: 12
+// // }
+// // // rectangularArea.width=10;
+// // type A = AreaNumber['height']  //lookup types
+// // type B = keyof AreaNumber;  // 'width' | 'height' ekta union type toyari korbe
+// // // bracket notation of object
+// // // const obj = {
+// // //     name: 'Nahid'
+// // // }
+// // // obj['name'];
+// // type Volume={
+// //     height: number;
+// //     width:number;
+// //     depth:number;
+// // };
+// // // type Area = {
+// // //     // [mapping] : type   //index signature
+// // //     // [key in 'height' | 'width'] : number
+// // //     // [key in keyof Volume] : number
+// // //     // dynamic
+// // //     [key in keyof Volume] : Volume[key]  //volume['height'] -->number;   volume['width'] -->number
+// // // }
+
+
+// // // Generic
+// // type Area<T> = {
+// //     // [key in keyof Volume] : Volume[key]  
+// //     // [key in keyof T] : T[key]
+// //     readonly [key in keyof T] : T[key]
 // // }
 
+// // // const area1: Area<{name: string}>={name: 'Nahid'}
+// // // const area1: Area<{name: string, age: number}>={name: 'Nahid', age: 12}
 
-// // Generic
-// type Area<T> = {
-//     // [key in keyof Volume] : Volume[key]  
-//     // [key in keyof T] : T[key]
-//     readonly [key in keyof T] : T[key]
-// }
+// // const area1: Area<{height: number, width : number, depth : number}> = {height: 12, width : 10, depth : 10}
 
-// // const area1: Area<{name: string}>={name: 'Nahid'}
-// // const area1: Area<{name: string, age: number}>={name: 'Nahid', age: 12}
-
-// const area1: Area<{height: number, width : number, depth : number}> = {height: 12, width : 10, depth : 10}
-
-// // area1.height = 15;   // readonly property changed or assign kora jabe na
+// // // area1.height = 15;   // readonly property changed or assign kora jabe na
 
 
-interface Person {
+// interface Person {
 
-    firstName: string;
+//     firstName: string;
   
-    lastName: string;
+//     lastName: string;
   
-  }
+//   }
   
   
   
-  function fullName<T extends Person>(person: T): string {
+//   function fullName<T extends Person>(person: T): string {
   
-    return `${person.firstName} ${person.lastName}`;
+//     return `${person.firstName} ${person.lastName}`;
   
-  }
+//   }
--- src/module5_5/utility.ts
+++ src/module5_5/utility.ts
@@ -1,5 +1,4 @@
 // pick(kono interface er property and type jodi onno type e use korte hoy)
-
 interface Person{
     name : string;
     email : string;
@@ -8,4 +7,8 @@ interface Person{
 // Pick keyword duita parameter ney(kotha theke use kobo, 'konta use korbo')
 type Email = Pick<Person, "email">
 type Contact = Pick<Person, "ContactNo" | "email">
+// Omit(kono interface theke kono property or type omit/bad hoye jabe)
+// Omit keyword duita parameter ney(kotha theke fele dibo, 'konta fele dibo')
+type email = Omit<Person, "email" | "ContactNo">
+
 
