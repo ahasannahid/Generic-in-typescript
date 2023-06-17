2e34474d514ce366b3c4c6643390d50889a80547
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-06-17 20:45:36 +0600

    partial and required type

--- src/module5_5/utility.ts
+++ src/module5_5/utility.ts
@@ -1,7 +1,7 @@
 // pick(kono interface er property and type jodi onno type e use korte hoy)
 interface Person{
     name : string;
-    email : string;
+    email ?: string;
     ContactNo : string;
 }
 // Pick keyword duita parameter ney(kotha theke use kobo, 'konta use korbo')
@@ -9,6 +9,13 @@ type Email = Pick<Person, "email">
 type Contact = Pick<Person, "ContactNo" | "email">
 // Omit(kono interface theke kono property or type omit/bad hoye jabe)
 // Omit keyword duita parameter ney(kotha theke fele dibo, 'konta fele dibo')
-type email = Omit<Person, "email" | "ContactNo">
+type Name = Omit<Person, "email" | "ContactNo">
+
+// Partial and Required(jokhn kono property optional thake)
+// to make al the properties be optional type we use partial
+type Optional = Partial<Person>
+
+// Required(reverse of Partial)
+type RequiredProperties = Required<Person>
 
 
