41b18ce55da0caa642a6be64b4fc5d9f3579d93e
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-06-17 21:40:44 +0600

    create class with method interface and a common model

---
+++ src/module5_5/advance.ts
@@ -0,0 +1,26 @@
+// properties
+interface Model<TData, TMethods>{
+    data : TData;
+    methods : TMethods;
+}
+interface IUser{
+    firstName : string;
+    lastName : string;
+}
+// method
+interface IMethods{
+   fullName() : string;
+}
+type model = Model<IUser, IMethods>;
+
+
+class User implements model{
+    data : IUser;
+    methods : IMethods;
+    constructor( firstName : string, lastName : string){
+        this.data = {firstName, lastName}
+        this.methods = {
+            fullName:()=>`${firstName} ${lastName}`
+        }
+    }
+}
--- src/module5_5/utility.ts
+++ src/module5_5/utility.ts
@@ -1,53 +1,54 @@
-// pick(kono interface er property and type jodi onno type e use korte hoy)
-interface Person{
-    name : string;
-    email ?: string;
-    ContactNo : string;
-}
-// Pick keyword duita parameter ney(kotha theke use kobo, 'konta use korbo')
-type Email = Pick<Person, "email">
-type Contact = Pick<Person, "ContactNo" | "email">
-// Omit(kono interface theke kono property or type omit/bad hoye jabe)
-// Omit keyword duita parameter ney(kotha theke fele dibo, 'konta fele dibo')
-type Name = Omit<Person, "email" | "ContactNo">
-
-// Partial and Required(jokhn kono property optional thake)
-// to make al the properties be optional type we use partial
-type Optional = Partial<Person>
-// Required(reverse of Partial)
-type RequiredProperties = Required<Person>
-
-// Readonly type
-
-const person:Readonly <Person>={
-    name : 'Nahid',
-    email : 'ahanhid123@gmail.com',
-    ContactNo : '01910613458'
-}
-
-// person.name = 'Ahasan'
-
-// Record type
-
-// type myObj = {
-//     a : string,
-//     b : string,
-//     c : string,
+// // pick(kono interface er property and type jodi onno type e use korte hoy)
+// interface Person{
+//     name : string;
+//     email ?: string;
+//     ContactNo : string;
 // }
-// using index signature
-// type myObj = {
-//     // [key : string] : string;
-//     [key : 'a' | 'b' | 'c'] : string;
+// // Pick keyword duita parameter ney(kotha theke use kobo, 'konta use korbo')
+// type Email = Pick<Person, "email">
+// type Contact = Pick<Person, "ContactNo" | "email">
+// // Omit(kono interface theke kono property or type omit/bad hoye jabe)
+// // Omit keyword duita parameter ney(kotha theke fele dibo, 'konta fele dibo')
+// type Name = Omit<Person, "email" | "ContactNo">
+
+// // Partial and Required(jokhn kono property optional thake)
+// // to make al the properties be optional type we use partial
+// type Optional = Partial<Person>
+// // Required(reverse of Partial)
+// type RequiredProperties = Required<Person>
+
+// // Readonly type
+
+// const person:Readonly <Person>={
+//     name : 'Nahid',
+//     email : 'ahanhid123@gmail.com',
+//     ContactNo : '01910613458'
 // }
 
-// Record Type implement(key er type ki hobe and key hobe kon type)
-// 'a' | 'b' | 'c']
-type myObj = Record<'a' | 'b' | 'c', string>
+// // person.name = 'Ahasan'
 
-const obj : myObj = {
-    a : '1',
-    b : '2',
-    c : '3',
-}
+// // Record type
+
+// // type myObj = {
+// //     a : string,
+// //     b : string,
+// //     c : string,
+// // }
+// // using index signature
+// // type myObj = {
+// //     // [key : string] : string;
+// //     [key : 'a' | 'b' | 'c'] : string;
+// // }
+
+// // Record Type implement(key er type ki hobe and key hobe kon type)
+// // 'a' | 'b' | 'c']
+// type myObj = Record<'a' | 'b' | 'c', string>
+
+// const obj : myObj = {
+//     a : '1',
+//     b : '2',
+//     c : '3',
+//     // d : '4'
+// }
 
 
